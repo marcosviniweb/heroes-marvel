374c7176506ef86fc68746213f95e7dae919f83c
Marcos Ribeiro <63163857+marcosviniweb@users.noreply.github.com>
2022-07-04 13:02:34 -0300

    Fix: Correção HTTPS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'http://gateway.marvel.com/v1/public',
+  apiURL: 'https://gateway.marvel.com/v1/public',
   pubkey: 'a3015785d416d3a471733a4b6469c4d6',
   pvtkey: '998ffb3bd3d087acad758a8219cffcf957fb84f2'
 };
